3ccc282e722366d14d3af62991642b737663775d
akihoho <117783214+akihoho@users.noreply.github.com>
2023-05-22 16:38:28 +0900

    Update anime-script.js

--- assets/js/anime-script.js
+++ assets/js/anime-script.js
@@ -2,14 +2,14 @@
   
     const ref = document.referrer;
     const url = location.hostname;
-    const spinner = document.getElementById('loading');
+//     const spinner = document.getElementById('loading');
     const currentNav = document.querySelector(".nav-list li:nth-child(1) a");
     console.log(currentNav);
 
     //console.log(ref,url);
-    if(ref.indexOf(url) != -1){
-      spinner.classList.add('from-local');
-    }else{
-      spinner.classList.add('loaded');
-      currentNav.classList.add('current');
-    }
+//     if(ref.indexOf(url) != -1){
+//       spinner.classList.add('from-local');
+//     }else{
+//       spinner.classList.add('loaded');
+//       currentNav.classList.add('current');
+//     }
