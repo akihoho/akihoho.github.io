f05fde1a5548fd1c261fcafc03b3a6f68b6ff2da
akihoho <117783214+akihoho@users.noreply.github.com>
2023-05-23 08:41:13 +0900

    Update anime-script.js

--- assets/js/anime-script.js
+++ assets/js/anime-script.js
@@ -2,9 +2,9 @@
   
     const ref = document.referrer;
     const url = location.hostname;
-    const spinner = document.getElementById('loading');
+    //const spinner = document.getElementById('loading');
     const currentNav = document.querySelector(".nav-list li:nth-child(1) a");
-    console.log(currentNav);
+    //console.log(currentNav);
 
     //console.log(ref,url);
     if(ref.indexOf(url) != -1){
